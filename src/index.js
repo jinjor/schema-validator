3080495b65c76c033795b2b4b14e9d386f134f6d
jinjor <jinjorweb@gmail.com>
2017-09-17 05:40:55 +0900

    roughly implement

--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
-module.exports = {};
+// utility
+const assign = obj => overrides => Object.assign({}, obj, overrides);
+
+// schema object
+function init(_validate) {
+  const schema = {};
+  schema._validate = _validate;
+  schema.map = map(schema);
+  schema.then = then(schema);
+  schema.validate = validate(schema);
+  return schema;
+}
+
+// conversion
+const map = schema => f => assign(schema, {
+  _validate: context => value => f(schema.value)
+});
+const then = schema => f => init(context => value => {
+  const newValue = schema._validate(value);
+  if (newValue instanceof Error) {
+    return newValue;
+  }
+  return f(context)(newValue);
+})
+
+// primitives
+const typeOf = typeName => init(context => value => {
+  if (typeof value === typeName) {
+    return value;
+  } else {
+    return new Error(value + ' is not a ' + typeName);
+  }
+});
+
+// validate
+const initialContext = {};
+const validate = schema => value => {
+  var newValue = schema._validate(initialContext)(value)
+  if (newValue instanceof Error) {
+    throw newValue;
+  }
+  return newValue;
+};
+
+// types
+const boolean = typeOf('boolean');
+const number = typeOf('number');
+const string = typeOf('string');
+const func = typeOf('function');
+const object = typeOf('object');
+const any = typeOf('any', context => value => true);
+
+module.exports = {
+  boolean: boolean,
+  number: number,
+  string: string,
+  func: func,
+  object: object,
+  any: any
+};
--- test/tests.js
+++ test/tests.js
@@ -1,11 +1,18 @@
-const schemaValidator = require('../src/index.js');
+const sv = require('../src/index.js');
 const chai = require('chai');
 const assert = chai.assert;
 
 describe('schema-validator', function() {
   describe('#validate()', function() {
-    it('should work', function() {
-      assert.fail();
+    it('should validate number', function() {
+      assert.equal(0, sv.number.validate(0));
+      assert.equal(1, sv.number.validate(1));
+      assert.equal(Infinity, sv.number.validate(Infinity));
+      assert.throws(() => sv.number.validate());
+      assert.throws(() => sv.number.validate(null));
+      assert.throws(() => sv.number.validate({}));
+      assert.throws(() => sv.number.validate(''));
+      assert.throws(() => sv.number.validate(() => {}));
     });
   });
 });
