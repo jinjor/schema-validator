6dbb0e5366390ba3601b99256a52750bf83d87e5
jinjor <jinjorweb@gmail.com>
2017-09-27 17:53:43 +0900

    make things more reusable

--- src/predefined.js
+++ src/predefined.js
@@ -1,56 +1,76 @@
 module.exports = function(original) {
+  const is = (message, isValid) => Schema.satisfy(isValid, 'should be ' + message);
+  const isnt = (message, isValid) => Schema.satisfy(isValid, 'should not be ' + message);
+  const truthy = () => is('truthy', value => value);
+  const falsy = () => is('falsy', value => !value);
+  const equal = (expected) => is(`equal to ${expected}`, value => expected === value);
+  const lt = (limit) => is(`less than ${limit}`, value => value < limit);
+  const gt = (limit) => is(`greater than ${limit}`, value => value > limit);
+  const min = (limit) => isnt(`less than ${limit}`, value => value >= limit);
+  const max = (limit) => isnt(`greater than ${limit}`, value => value <= limit);
+  const typeOf = (typeName) => {
+    const a = /[aeiou]/.test(typeName.charAt(0)) ? 'an' : 'a';
+    return is(`${a} ${typeName}`, value => typeof value === typeName);
+  };
+  const instanceOf = (cls) => {
+    return is(`instance of ${cls.name}`, value => value instanceof cls);
+  };
+  const boolean = () => typeOf('boolean');
+  const number = () => typeOf('number');
+  const string = () => typeOf('string');
+  const func = () => typeOf('function');
+  const object = () => Schema.check(typeOf('object').isnt('null', value => value !== null))
   const Schema = original.extend({
     // Satisfaction
     is(message, isValid) {
-      return this.next(Schema.satisfy(isValid, 'should be ' + message));
+      return this.next(is(message, isValid));
     },
     isnt(message, isValid) {
-      return this.next(Schema.satisfy(isValid, 'should not be ' + message));
+      return this.next(isnt(message, isValid));
     },
     // Comparison
     truthy() {
-      return this.is('truthy', value => value);
+      return this.next(truthy());
     },
     falsy() {
-      return this.is('falsy', value => !value);
+      return this.next(falsy());
     },
     equal(expected) {
-      return this.is(`equal to ${expected}`, value => expected === value);
+      return this.next(equal(expected));
     },
     lt(limit) {
-      return this.is(`less than ${limit}`, value => value < limit);
+      return this.next(lt(limit));
     },
     gt(limit) {
-      return this.is(`greater than ${limit}`, value => value > limit);
+      return this.next(gt(limit));
     },
     min(limit) {
-      return this.isnt(`less than ${limit}`, value => value >= limit);
+      return this.next(min(limit));
     },
     max(limit) {
-      return this.isnt(`greater than ${limit}`, value => value <= limit);
+      return this.next(max(limit));
     },
     // Types
     typeOf(typeName) {
-      const a = /[aeiou]/.test(typeName.charAt(0)) ? 'an' : 'a';
-      return this.is(`${a} ${typeName}`, value => typeof value === typeName);
+      return this.next(typeOf(typeName));
     },
     instanceOf(cls) {
-      return this.is(`instance of ${cls.name}`, value => value instanceof constructorFunc);
+      return this.next(instanceOf(cls));
     },
     boolean() {
-      return this.typeOf('boolean');
+      return this.next(boolean());
     },
     number() {
-      return this.typeOf('number');
+      return this.next(number());
     },
     string() {
-      return this.typeOf('string');
+      return this.next(string());
     },
     func() {
-      return this.typeOf('function');
+      return this.next(func());
     },
     object() {
-      return this.is(`an object`, value => typeof value === 'object' && value !== null);
+      return this.next(object());
     },
     date() {
       return this.instanceOf(Date);
@@ -76,12 +96,10 @@ module.exports = function(original) {
     },
     // Array(-like)
     minLength(limit) {
-      // return Schema.check(Schema.key('length').min(limit));
-      return Schema.check(Schema.key('length', Schema.satisfy(v => v >= limit)));
+      return Schema.check(Schema.key('length', min(limit)));
     },
     maxLength(limit) {
-      // return Schema.check(Schema.key('length').max(limit));
-      return Schema.check(Schema.key('length', Schema.satisfy(v => v <= limit)));
+      return Schema.check(Schema.key('length', max(limit)));
     },
     field(key, valueSchema, checkerSchema) {
       if (checkerSchema) {
